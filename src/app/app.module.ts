4d24eacbd402481246d96538d6ab5c19b22a2ee2
Robert Strzelczyk <123934951+rStrzelczyk98@users.noreply.github.com>
2023-08-17 11:38:45 +0200

    add/edit comment (#2)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { CommentCardComponent } from './comment-card/comment-card.component';
 import { CommentGroupComponent } from './comment-group/comment-group.component';
+import { CommentInputComponent } from './comment-input/comment-input.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
     CommentCardComponent,
-    CommentGroupComponent
+    CommentGroupComponent,
+    CommentInputComponent,
   ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule, ReactiveFormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
